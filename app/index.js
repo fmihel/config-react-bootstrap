e54d70536d75e57ce7e62f914f711371bb1b9d28
unknown <fmihel76@gmail.com>
2020-03-19 22:02:19 +0300

    change struct, add collapsed panel

--- app/index.js
+++ app/index.js
@@ -2,7 +2,8 @@ import './scss/main.scss';
 import { DOM } from 'fmihel-browser-lib';
 import React from 'react';
 import ReacDOM from 'react-dom';
-import { App } from 'components/App.jsx';
+import { App } from './App.jsx';
+import '@fortawesome/fontawesome-free/js/all';
 
 $(() => {
     ReacDOM.render(<App/>, DOM('#app'));
